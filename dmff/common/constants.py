e635c3f64dea3fb2398a2e4087e190eab8dcec68
Zhu, Jia-Xin <j.zhu@fz-juelich.de>
2026-09-08 15:29:22 +0200

    Derive physical constants from scipy.constants instead of hard-coded values

--- dmff/common/constants.py
+++ dmff/common/constants.py
@@ -1,10 +1,22 @@
 import numpy as np
+from scipy import constants
 
-DIELECTRIC = 1389.35455846
+j2ev = constants.physical_constants["joule-electron volt relationship"][0]
+# kJ/mol to eV/particle
+energy_coeff = j2ev * constants.kilo / constants.Avogadro
+# vacuum electric permittivity in eV^-1 * angstrom^-1
+epsilon = constants.epsilon_0 / constants.elementary_charge * constants.angstrom
+# qqrd2e = 1 / (4 * np.pi * epsilon)
+# # eV
+# dielectric = 1 / (4 * np.pi * epsilon)
+# kJ/mol
+DIELECTRIC = 1 / (4 * np.pi * epsilon) / energy_coeff
+# DIELECTRIC = 1389.35455846
 SQRT_PI = np.sqrt(np.pi)
 
 # units
-EV2KJ = 96.48530749925791
+EV2KJ = 1 / energy_coeff  # 96.48533212331001
 
-A2NM = 0.1
+A2NM = constants.angstrom / constants.nano  # 0.1
 
+__all__ = ["DIELECTRIC", "SQRT_PI", "EV2KJ", "A2NM"]
